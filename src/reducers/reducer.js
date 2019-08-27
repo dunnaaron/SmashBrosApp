724a305a819e4c8099699e819e82a360b301350b
Aaron Dunn <aaron.dunn@accenture.com>
2019-08-27 10:31:19 -0400

    updated babel and reducer to new pattern

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,12 +1,9 @@
-export const DEFAULT_STATE = {
-  helloWorld: 'initial state'
-}
+export const DEFAULT_STATE = { }
 
 export default (state = DEFAULT_STATE, action = {}) => {
   switch (action.type) {
     case 'SET_CURRENT_FIGHTER':
       return { ...state, selectedCurrentFighter: action.value };
-      // return Object.assign({}, state, { selectedCurrentFighter: action.value })
     default:
       return state;
   }
