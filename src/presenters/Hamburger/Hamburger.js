8150ff404ec47d90051d147244b6aaf90644ba14
dunnaaron <dunnac1@gmail.com>
2020-01-07 14:50:18 -0500

    hamburger animation complete

--- src/presenters/Hamburger/Hamburger.js
+++ src/presenters/Hamburger/Hamburger.js
@@ -1,23 +1,15 @@
 import React, { useState } from 'react'
-import './hamburger.css'
-
-// const Hamburger = () => {
-//   return <div class='hamburger'>
-//       <div className='bar top' />
-//       <div className='bar middle' />
-//       <div className='bar bottom' />
-//   </div>
-// }
+import './hamburger.scss'
 
 const Hamburger = () => {
   const [isExpanded, setExpanded] = useState(false)
 
   const expanded = isExpanded ? 'expanded' : '';
 
-  return <button className='hamburger' onClick={() => setExpanded(!isExpanded)}>
-      <div className={`bar top ${expanded}`}/>
-      <div className={`bar middle ${expanded}`}/>
-      <div className={`bar bottom ${expanded}`}/>
+  return <button className={`hamburger ${expanded}`} onClick={() => setExpanded(!isExpanded)}>
+      <div className={`bar-top ${expanded}`}/>
+      <div className={`bar-middle ${expanded}`}/>
+      <div className={`bar-bottom ${expanded}`}/>
   </button>
 }
 
