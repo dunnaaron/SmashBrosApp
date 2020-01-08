7301d11b47101137c9ea523782b7a02ddda6e9f1
dunnaaron <dunnac1@gmail.com>
2020-01-08 10:27:41 -0500

    mobile nav drawer animations and linking

--- src/presenters/MobileNav/MobileNav.js
+++ src/presenters/MobileNav/MobileNav.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './mobile-nav.scss'
 
 const MobileNav = ({navOpen}) => (
   <div className={`${navOpen && 'modal'}`}>
+    <div className='nav-links'>
+      <Link className='nav-item' to='/'>Home</Link>
+      <Link className='nav-item' to='/'>Calculators</Link>
+      <Link className='nav-item' to='/'>Register</Link>
+      <Link className='nav-item' to='/About'>About</Link>
+    </div>
   </div>
 )
 
